9f2db41c90d0eb0f95ef0e112d9a15b5d13449d9
abineshprabhakaran <abineshprabhakaran97@gmail.com>
2020-06-23 20:13:36 +0530

    Animation changes

---
+++ js/custom.js
@@ -0,0 +1,39 @@
+(function () {
+    var myDiv = document.getElementById("world"),
+        show = function () {
+            myDiv.style.display = "block";
+            setTimeout(hide, 5000);
+        },
+        hide = function () {
+            myDiv.style.display = "none";
+        };
+    show();
+})();
+
+$(document).ready(function () {
+    setTimeout(function () {
+        $("#mainPage").show();
+    }, 5000);
+});
+
+$(window).on("scroll", function () {
+    if ($(window).scrollTop() > 50) {
+        $(".header").addClass("activeHeader");
+    } else {
+        $(".header").removeClass("activeHeader");
+    }
+});
+$(window).on("scroll", function () {
+    if ($(window).scrollTop() > 50) {
+        $(".logoContainer").addClass("logoContainerBlock");
+    } else {
+        $(".logoContainer").removeClass("logoContainerBlock");
+    }
+});
+$(window).on("scroll", function () {
+    if ($(window).scrollTop() > 50) {
+        $(".jusEnd").addClass("jusBetween");
+    } else {
+        $(".jusEnd").removeClass("jusBetween");
+    }
+});
